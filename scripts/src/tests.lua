643acda598917571847bc9e9bd7837e56ebd02f6
starlight_world <43389528+matt-dawidowicz@users.noreply.github.com>
2026-09-03 17:58:02 -0400

    tests: register CD-i audio specification tests

--- scripts/src/tests.lua
+++ scripts/src/tests.lua
@@ -65,6 +65,7 @@ project("mametests")
 		MAME_DIR .. "tests/main.cpp",
 		MAME_DIR .. "tests/emu/machine/scc68070.cpp",
 		MAME_DIR .. "tests/emu/philips/cdicdic.cpp",
+		MAME_DIR .. "tests/emu/philips/cdicdic_audio.cpp",
 		MAME_DIR .. "tests/emu/philips/mcd212_video.cpp",
 		MAME_DIR .. "tests/emu/philips/cdidvc.cpp",
 		MAME_DIR .. "tests/emu/philips/cdidvc_timing.cpp",
