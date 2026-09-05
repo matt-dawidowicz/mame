054abb66b8f6b720fe925fd1bbd9eb1300aa7e68
starlight_world <43389528+matt-dawidowicz@users.noreply.github.com>
2026-09-05 18:17:51 -0400

    ci: compile out disabled DVC decode hashes

--- scripts/cdi-diagnostic-optimization-apply.py
+++ scripts/cdi-diagnostic-optimization-apply.py
@@ -30,7 +30,43 @@ new_audio = '''#if (VERBOSE & LOG_AUDIO)
 \t\t\t\t++m_audio_output_nonzero;
 #endif
 '''
-replace_once(old_audio, new_audio, "DVC audio diagnostic hash")
+replace_once(old_audio, new_audio, "DVC audio output diagnostic hash")
+
+old_decode_hash_decl = '''\t\tuint32_t hash = 2166136261U;
+\t\tsize_t const values = size_t(samples->count) * 2;
+'''
+new_decode_hash_decl = '''#if (VERBOSE & LOG_AUDIO)
+\t\tuint32_t hash = 2166136261U;
+#endif
+\t\tsize_t const values = size_t(samples->count) * 2;
+'''
+replace_once(old_decode_hash_decl, new_decode_hash_decl, "DVC decoded PCM hash declaration")
+
+old_decode_hash_update = '''\t\t\tint16_t const pcm = cdi_dvc::quantize_plm_audio_sample(float(filtered));
+\t\t\thash = cdi_dvc::hash_pcm16_sample(hash, pcm);
+\t\t\tm_audio_pcm_queue.push_back(pcm);
+'''
+new_decode_hash_update = '''\t\t\tint16_t const pcm = cdi_dvc::quantize_plm_audio_sample(float(filtered));
+#if (VERBOSE & LOG_AUDIO)
+\t\t\thash = cdi_dvc::hash_pcm16_sample(hash, pcm);
+#endif
+\t\t\tm_audio_pcm_queue.push_back(pcm);
+'''
+replace_once(old_decode_hash_update, new_decode_hash_update, "DVC decoded PCM hash update")
+
+old_decode_hash_log = '''\t\tLOGMASKED(LOG_AUDIO, "%s: DVC AUDIO decoded frames=%u frame_samples=%u total_samples=%u event=%u status=%02x fnv=%08x\\n",
+\t\t\t\tmachine().describe_context(), m_audio_decoded_frames, samples->count,
+\t\t\t\tm_audio_decoded_samples, m_audio_decode_events,
+\t\t\t\tm_audio_backend_status, hash);
+'''
+new_decode_hash_log = '''#if (VERBOSE & LOG_AUDIO)
+\t\tLOGMASKED(LOG_AUDIO, "%s: DVC AUDIO decoded frames=%u frame_samples=%u total_samples=%u event=%u status=%02x fnv=%08x\\n",
+\t\t\t\tmachine().describe_context(), m_audio_decoded_frames, samples->count,
+\t\t\t\tm_audio_decoded_samples, m_audio_decode_events,
+\t\t\t\tm_audio_backend_status, hash);
+#endif
+'''
+replace_once(old_decode_hash_log, new_decode_hash_log, "DVC decoded PCM hash log")
 
 old_video = '''\t\t\tpixels[out_x] = color;
 \t\t\tuint8_t const r = uint8_t(color >> 16);
