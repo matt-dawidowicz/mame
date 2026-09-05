d17fd2ab7e12dc4b2cf1cb694f1f0265d5b038c8
starlight_world <43389528+matt-dawidowicz@users.noreply.github.com>
2026-09-05 18:25:12 -0400

    philips/dvc: isolate PL_MPEG implementation

---
+++ src/mame/philips/cdidvc_plmpeg.cpp
@@ -0,0 +1,8 @@
+// license:BSD-3-Clause
+// copyright-holders:Matt Jordan
+
+// Keep the single-header decoder implementation in a tiny, stable
+// translation unit so ordinary DVC scheduling/register edits compile faster.
+#define PLM_NO_STDIO
+#define PL_MPEG_IMPLEMENTATION
+#include "../../../3rdparty/pl_mpeg/pl_mpeg.h"
