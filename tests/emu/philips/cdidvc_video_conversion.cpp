d3153ef2618d4509798d77a212d9dfabcf46f2a0
starlight_world <43389528+matt-dawidowicz@users.noreply.github.com>
2026-09-10 01:01:30 -0400

    test(cdi): cover DVC sync clock and VCD presentation cadence

--- tests/emu/philips/cdidvc_video_conversion.cpp
+++ tests/emu/philips/cdidvc_video_conversion.cpp
@@ -6,6 +6,7 @@
 #include <cstdint>
 
 #include "catch.hpp"
+#include "cdidvc_presentation.h"
 
 #define PLM_NO_STDIO
 #include "../../../3rdparty/pl_mpeg/pl_mpeg.h"
@@ -58,4 +59,57 @@ TEST_CASE("CD-i DVC PL_MPEG 4-byte conversion preserves RGB values", "[emu][phil
 		REQUIRE(argb[pixel * 4 + 2] == rgb[pixel * 3 + 1]);
 		REQUIRE(argb[pixel * 4 + 3] == rgb[pixel * 3 + 2]);
 	}
+}
+
+TEST_CASE("CD-i DVC VCD mode uses the 15 MHz to 13.5 MHz horizontal cadence", "[emu][philips][dvc][video][vcd]")
+{
+	REQUIRE_FALSE(cdi_dvc::vcd_pixel_clock_enabled(0));
+	REQUIRE(cdi_dvc::vcd_pixel_clock_enabled(1));
+
+	REQUIRE(cdi_dvc::video_output_width(0, false) == 0);
+	REQUIRE(cdi_dvc::video_output_width(352, false) == 704);
+	REQUIRE(cdi_dvc::video_output_width(352, true) == 782);
+	REQUIRE(cdi_dvc::video_output_width(345, true) == 767);
+
+	for (unsigned source = 0; source < 16; ++source)
+	{
+		REQUIRE(cdi_dvc::video_source_x_for_output(source * 2, 16, false) == source);
+		REQUIRE(cdi_dvc::video_source_x_for_output(source * 2 + 1, 16, false) == source);
+	}
+
+	unsigned previous = 0;
+	for (unsigned output = 0; output < cdi_dvc::video_output_width(352, true); ++output)
+	{
+		unsigned const source = cdi_dvc::video_source_x_for_output(output, 352, true);
+		INFO("output=" << output << " source=" << source);
+		REQUIRE(source < 352);
+		REQUIRE(source >= previous);
+		REQUIRE(source <= previous + 1);
+		previous = source;
+	}
+	REQUIRE(cdi_dvc::video_source_x_for_output(781, 352, true) == 351);
+}
+
+TEST_CASE("CD-i DVC synchronous video clock follows programmed GEN_SYSCR", "[emu][philips][dvc][video][avsync]")
+{
+	auto clock = cdi_dvc::select_video_sync_clock(
+		true, 45'123, true, 1'000, 100);
+	REQUIRE(clock.valid);
+	REQUIRE(clock.external_dclk);
+	REQUIRE(clock.clock90 == 90'246);
+
+	clock = cdi_dvc::select_video_sync_clock(
+		false, 145, true, 1'000, 100);
+	REQUIRE(clock.valid);
+	REQUIRE_FALSE(clock.external_dclk);
+	REQUIRE(clock.clock90 == 1'090);
+
+	clock = cdi_dvc::select_video_sync_clock(
+		false, 145, false, 0, 0);
+	REQUIRE_FALSE(clock.valid);
+
+	clock = cdi_dvc::select_video_sync_clock(
+		true, 0xffffffffU, false, 0, 0);
+	REQUIRE(clock.valid);
+	REQUIRE(clock.clock90 == cdi_dvc::MPEG_TIMESTAMP_MASK - 1U);
 }
